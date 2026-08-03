c9715d388ea050baea40097235430a1164a33475
Samarth Shrivastav <samarthsrivastav740@gmail.com>
2026-08-03 21:10:19 +0530

    Update file paths

--- python/utils/file_paths.py
+++ python/utils/file_paths.py
@@ -18,10 +18,25 @@ EV_SALES_BY_CAT_AND_MAKERS=REFERENCE_FOLDER/"ev_sales_by_makers_and_cat_15-24.cs
 EV_VEHICLE_MASTER=REFERENCE_FOLDER/"ev_vehicle_master.csv"
 OPERATIONAL_PCS_PATH=REFERENCE_FOLDER/"OperationalPC.csv"
 STATE_COORDINATES_PATH=REFERENCE_FOLDER/"state_coordinates.csv"
+STATE_POPULATION_PATH = REFERENCE_FOLDER/"state_population.csv" 
 
 # Simulated Dataset paths
 DIM_CUSTOMER = SIMULATED_FOLDER/"dim_customer.csv"
 DIM_CHARGING_STATION = SIMULATED_FOLDER/"dim_charging_station.csv"
 FACT_CHARGING_SESSIONS = SIMULATED_FOLDER/"fact_charging_sessions.csv"
+
+# RAW tables dictionary
+RAW_TABLES = {
+    "state_ev_registrations": STATE_EV_REGISTRATIONS,
+    "ev_sales_by_makers": EV_SALES_BY_CAT_AND_MAKERS,
+    "ev_vehicle_master": EV_VEHICLE_MASTER,
+    "operational_pcs": OPERATIONAL_PCS_PATH,
+    "state_coordinates": STATE_COORDINATES_PATH,
+    "state_population": STATE_POPULATION_PATH,
+
+    "dim_customer": DIM_CUSTOMER,
+    "dim_charging_station": DIM_CHARGING_STATION,
+    "fact_charging_sessions": FACT_CHARGING_SESSIONS
+}
 
 
